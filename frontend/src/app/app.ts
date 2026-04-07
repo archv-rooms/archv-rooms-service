539e0e4545643e8be7422279b90afb455afd16f4
Beyond-Isaac <isaacvcarv4lho@gmail.com>
2026-04-07 02:06:07 -0300

    chore: adiciona gitignore e atualiza projeto

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,5 +1,7 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './shared/components/header/header.component';
+imports: [RouterOutlet, HeaderComponent]
 
 @Component({
   selector: 'app-root',
