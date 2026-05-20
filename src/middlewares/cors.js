7f25ee430b2b119b14bc140cc73e8c05372178c6
cofeezz <dokke.donkey@gmail.com>
2026-05-20 11:29:17 -0300

    modify cors .js for tests

--- src/middlewares/cors.js
+++ src/middlewares/cors.js
@@ -1,20 +1,30 @@
 import cors from 'cors'
 
-const allowedOrigins = [ 
-  'https://archv-rooms-web.vercel.app/login',
-  'https://archv-rooms-web.vercel.app/pricing',
-  'https://archv-rooms-web.vercel.app'
-]
-
 const corsMiddleware = cors({
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  },
+  origin: true, // Permite todas as origens
   credentials: true
 })
 
 export default corsMiddleware
+
+
+// import cors from 'cors'
+
+// const allowedOrigins = [ 
+//   'https://archv-rooms-web.vercel.app/login',
+//   'https://archv-rooms-web.vercel.app/pricing',
+//   'https://archv-rooms-web.vercel.app'
+// ]
+
+// const corsMiddleware = cors({
+//   origin: (origin, callback) => {
+//     if (!origin || allowedOrigins.includes(origin)) {
+//       callback(null, true)
+//     } else {
+//       callback(new Error('Not allowed by CORS'))
+//     }
+//   },
+//   credentials: true
+// })
+
+// export default corsMiddleware
