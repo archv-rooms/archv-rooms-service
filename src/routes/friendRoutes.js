a5ce1af215b71cba595787b11f42cac58335c422
andreemartinns <andreemartinns2@gmail.com>
2026-06-23 15:04:51 -0300

    feat(amigos): adiciona rota GET /activity

--- src/routes/friendRoutes.js
+++ src/routes/friendRoutes.js
@@ -5,8 +5,9 @@ import authMiddleware from '../middlewares/authMiddleware.js'
 const router = express.Router()
 
 router.get('/search', authMiddleware, friendController.searchUsers)
-router.get('/', authMiddleware, friendController.getFriends)
+router.get('/activity', authMiddleware, friendController.getFriendsActivity)
 router.get('/pending', authMiddleware, friendController.getPendingRequests)
+router.get('/', authMiddleware, friendController.getFriends)
 router.post('/request', authMiddleware, friendController.sendFriendRequest)
 router.patch('/:id/respond', authMiddleware, friendController.respondFriendRequest)
 router.delete('/:id', authMiddleware, friendController.removeFriend)
